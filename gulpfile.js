2f74b5d39fa422b73eb8be512bf2c9d6911c4020
Albert <abessilin@bk.ru>
2020-05-07 18:38:13 +0500

    modifid

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
-const gulp = require('gulp');
-const browserSync = require('browser-synk').create();
-
-gulp.task('hello', function(done) {
-  console.log('Привет, мир!');
-  done();
-})
+const {src,dest,watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    gulp.watch("./sass/**/*.sass", serveSass);
+    gulp.watch("./js/*.js").on('change', browserSync.reload);
+};
+
+function serveSass() {
+    return src("./sass/*.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
