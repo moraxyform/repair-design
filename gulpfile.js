6d28b4142dfdadf55e2b69f858552e1b14c6888d
Albert <abessilin@bk.ru>
2020-05-09 19:40:16 +0500

    feat: add normalize, grid, variables, interface, header styles in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,25 @@
-const {src,dest,watch} = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
+ const {src,dest,watch} = require('gulp');
+ const browserSync = require('browser-sync').create();
+ const sass = require('gulp-sass');
 
-// Static server
-function bs() {
-    serveSass();
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    watch("./*.html").on('change', browserSync.reload);
-    watch("./sass/**/*.sass", serveSass);
-    watch("./js/*.js").on('change', browserSync.reload);
-};
+ // Static server
+ function bs() {
+     serveSass();
+     browserSync.init({
+         server: {
+             baseDir: "./"
+         }
+     });
+     watch("./*.html").on('change', browserSync.reload);
+     watch("./sass/**/*.sass", serveSass);
+     watch("./js/*.js").on('change', browserSync.reload);
+ };
 
-function serveSass() {
-    return src("./sass/*.scss")
-        pipe(sass())
-        pipe(dest("./css"))
-        pipe(browserSync.stream());
-};
+ function serveSass() {
+     return src("./sass/**/*.sass")
+         pipe(sass())
+         pipe(dest("./css"))
+         pipe(browserSync.stream());
+ };
 
-exports.serve = bs;
+ exports.serve = bs;
