da85fb05c6b76da4b1ee573e2fd06fc912fad722
Albert <abessilin@bk.ru>
2020-05-16 19:39:16 +0500

    feat: added jquery

--- js/main.js
+++ js/main.js
@@ -1,15 +1,48 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible')
-  }
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible')
+//   }
 
-  modalBtn.forEach(element =>{
-    element.addEventListener('click', switchModal);
+//   modalBtn.forEach(element =>{
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBtn.addEventListener('click', switchModal)
+
+//   window.onclick = (event) => {
+//     if (event.target == modal) {
+//       modal.classList.toggle('modal--visible')
+//     }
+//   }
+//   document.addEventListener('keyup', function(e) {
+//     if(e.keyCode === 27) {
+//       modal.classList.remove('modal--visible')
+//     }
+//   })
+// });
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function() {
+    modal.toggleСlass('modal--visible');
   });
-
-  closeBtn.addEventListener('click', switchModal)
-
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible')
+  });
+  window.on('click', function(e) {
+    if(e.target == modal) {
+    modal.classList.toggle('modal--visible')
+    }
+  })
+  document.on('keyup', function(e) {
+    if(e.keyCode === 27) {
+      modal.classList.remove('modal--visible')
+    }
+  })
 });
